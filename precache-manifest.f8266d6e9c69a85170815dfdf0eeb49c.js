3c690cb09ec9a3f949b530586bf7561ad21a1ac6
Arman Aliani <armanaliani@gmail.com>
2020-08-13 15:42:13 -0400

    Updates

---
+++ precache-manifest.f8266d6e9c69a85170815dfdf0eeb49c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d22bcbde98b1b8aae3c8a6421098cadb",
+    "url": "/project5/index.html"
+  },
+  {
+    "revision": "aa8cb60f15c4ab1a2c6d",
+    "url": "/project5/static/css/main.52f8eb32.chunk.css"
+  },
+  {
+    "revision": "2809dec080a94737e888",
+    "url": "/project5/static/js/2.ccf758d0.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/project5/static/js/2.ccf758d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa8cb60f15c4ab1a2c6d",
+    "url": "/project5/static/js/main.f1fb457c.chunk.js"
+  },
+  {
+    "revision": "dada8958919365f6b89c",
+    "url": "/project5/static/js/runtime-main.62befbb1.js"
+  }
+]);
